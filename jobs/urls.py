011975d3657e0ab0cba703adeeadff4c07570409
MykhailoVV <vereshchaka.m@gmail.com>
2022-02-18 19:09:05 +0200

    Add Account APP

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import JobsListView, JobDetailView
+from .views import JobsListView, JobDetailView, JobCreateView, JobUpdateView, JobDeleteView
 
 urlpatterns = [
+    path('job/<int:pk>/delete/', JobDeleteView.as_view(), name='job_delete'),
+    path('job/<int:pk>/edit/', JobUpdateView.as_view(), name='job_edit'),
+    path('job/new/', JobCreateView.as_view(), name='job_new'),
     path('job/<int:pk>/', JobDetailView.as_view(), name='job_detail'),
     path('', JobsListView.as_view(), name='home'),
 ]
